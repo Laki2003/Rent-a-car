d40485de6b5295b75021720f108f3dc2b30f0c34
Laki2003 <61576180+Laki2003@users.noreply.github.com>
2020-05-19 21:14:02 +0200

    Pocetak backend-a
    Podesavanje aplikacije/sajta za backend i zavrsetak naslovne stranice

---
+++ Rent/config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://lazar:polovina@cluster0-cozh5.mongodb.net/test?retryWrites=true&w=majority'
+}
---
+++ public/app.js
@@ -0,0 +1,20 @@
+function menuBarClick(x) {
+    if($(".cross").hasClass("change")){
+        $("header").css({"background-color" : "black", "color":"white", "height":"auto", "overflow":"hidden"});
+        $(".nav-header a").css({"color":"white"});
+        $(".mobile-nav-item").css({"display":"none"});
+        $("body").css({"overflow":"scroll"});
+     }
+   else{  
+       $("body").css({"overflow":"hidden"})
+        $("header").css({"background-color" : "white", "color":"black", "height":"100%",  "overflow-y":"scroll"});
+       $(".nav-header a").css({"color":"black"});
+       $(".mobile-nav-item").css({"display":"block"});}
+    $(".mobile-nav-item a").css({"color":"black", "display":"block"});
+    x.classList.toggle("change");
+
+    $("#mobileid").toggleClass("mobile");
+
+}
+
+
