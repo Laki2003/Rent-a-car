11c14af96e4853b21313910ba7d37a0fd8ac7abe
Laki2003 <61576180+Laki2003@users.noreply.github.com>
2020-05-24 18:37:55 +0200

    Passport Register Authentication
    Dodavanje registracije na backendu.

---
+++ Rent/config/passport.js
@@ -0,0 +1,74 @@
+var passport = require('passport');
+var Korisnik = require('../models/korisnik');
+
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.serializeUser(function(korisnik, done){
+    done(null, korisnik.id);
+
+});
+
+passport.deserializeUser(function(id, done){
+    Korisnik.findById(id, function(err, korisnik){
+        done(err, korisnik);
+
+    });
+});
+
+passport.use('local.register', new LocalStrategy({
+usernameField: 'email',
+passwordField: 'password',
+passReqToCallback: true    
+}, function(req, email, password,done){
+    var username = req.body.username;
+  var passwordd = req.body.password;
+  var emaill = req.body.email;
+  var confirmpassword = req.body.confirm;
+  var errors = [];
+  
+  if(username.length == 0 || passwordd.length == 0 || emaill.length == 0  || confirmpassword.length == 0 )
+  {
+errors.push({message: "Polja su prazna"});
+  }
+if(passwordd.length<6)
+{
+    errors.push({message: "Sifra mora da bude dugacka najmanje 6 karaktera"});
+}
+if(passwordd != confirmpassword)
+{
+    errors.push({message: "Sifre se ne podudaraju"});
+}
+
+
+if(errors.length>0)
+{
+    var messages = [];
+    errors.forEach(function(error){
+        messages.push(error.message);
+    });
+    return done(null, false, req.flash('error', messages));
+}
+
+Korisnik.findOne({$or: [{'email': email}, {'username': username}]}, function(err, korisnik){
+   
+    if(err){
+        return done(err);
+    }
+    if(korisnik)
+    {
+        return done(null, false, {message: 'Email / Korisnicko ime je vec u upotrebi'});
+    }
+    var newKorisnik = new Korisnik();
+    newKorisnik.username = username;
+    newKorisnik.email = email;
+    newKorisnik.password = newKorisnik.encryptPassword(password);
+    newKorisnik.save(function(err, result){
+        if(err){
+            return done(err);
+        }
+     
+        return done(null, newKorisnik);
+    });
+});
+}));
+
--- Rent/routes/users.js
+++ Rent/routes/users.js
@@ -1,12 +1,66 @@
 var express = require('express');
 var router = express.Router();
+var csrf = require('csurf');
+var passport = require('passport');
+
+
+var csrfProtection = csrf();
+
+
+
+router.use(csrfProtection);
+
+
+
+
+
+router.get('/profile', isLoggedIn,  function(req, res, next){
+    
+     res.render('korisnik/profile');
+   
+})
+router.get('/logout', isLoggedIn,  function(req, res, next){
+  req.user = null;
+    req.logout();
+  res.redirect('/');
+});
+router.use('/', notLoggedIn, function(req, res, next){
+  next();
+})
 
 router.get('/register', function(req, res, next){
-     res.render('korisnik/register');
-   });
+  var messages = req.flash('error')
+   res.render('korisnik/register', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length >0});
+ });
+ 
+ router.post('/register', passport.authenticate('local.register', {
+ successRedirect: '/users/profile',
+  failureRedirect: '/users/register',
+   failureFlash: true
+ }))
+ router.get('/login', function(req, res, next){
+  res.render('korisnik/login');
+ });
 
-   router.get('/login', function(req, res, next){
-    res.render('korisnik/login');
-   });
 
-   module.exports = router;
+
+
+ module.exports = router;
+
+function isLoggedIn(req, res, next)
+{
+if(req.isAuthenticated())
+{
+  return next();
+}
+res.redirect('/');
+}
+ 
+ function notLoggedIn(req, res, next)
+ {
+   if(!req.isAuthenticated())
+   {
+     return next();
+   }
+   res.redirect('/');
+ }
