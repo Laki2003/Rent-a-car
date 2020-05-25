a6d9131ba6159d19c3742a4031aee23bd001b357
Kraguljac23 <61576481+Kraguljac23@users.noreply.github.com>
2020-05-25 17:49:40 +0200

    Pretraga i animirano brojanje

--- Rent/routes/index.js
+++ Rent/routes/index.js
@@ -1,13 +1,130 @@
 var express = require('express');
 var router = express.Router();
-
+var mongo = require('mongodb');
+var assert = require('assert');
+var mongoose = require('mongoose');
+var Booking= require('../models/booking');
+var Auto = require('../models/auto');
+var Lokacija = require('../models/lokacija');
 
 
  var url = require('../config/keys').MongoURI;
+
+ router.get('/', async function(req, res, next) {
+  var Greska = req.flash('greska');
+  var success = req.flash('success');
+var lokacija = await Lokacija.find().count();
+var auto = await Auto.find().count();
+var booking = await Booking.find().count();
+    res.render('index', {Greska: Greska, lokacija:lokacija, auto: auto, booking: booking, 
+    success: success});
  
-router.get('/', function(req, res, next) {
-    res.render('index');
+
+})
+router.get('/search',  function(req, res, next) {
+  const {people, location, cenaod, cenado, marka} =  req.query;
+  var errors = [];
+  const {from, to} = req.query
+var p = Number(people), price1 = Number(cenaod), price2= Number(cenado);
+  var date1 = new Date(from);
+  var date2 = new Date(to);
+  var number = date2.getTime() - date1.getTime();
+var today = new Date();
+  if(from.length==0 || to.length==0)
+{
+  
+  errors.push({message: 'Morate uneti datume!'});
+  req.flash('greska', errors);
+  res.redirect('/');
+}
+else if(number<0)
+{
+  errors.push({message: 'Pogresno uneti datumi!'});
+  req.flash('greska', errors);
+  res.redirect('/');
+}
+else if(date1.getTime()-today.getTime()<0)
+{
+  errors.push({message: "Uneli ste datume iz proslosti"});
+  req.flash('greska', errors);
+  res.redirect('/');
+}
+else {
+  if(!people)
+  {
+  p = 0;
+  }
+
+   if (!cenaod)
+  {
+    price1 = 0;
+  }
+
+ if (!cenado)
+{
+  price2  = 100000;
+}
+  var resultArray = [];
+  mongo.connect(url, function(err, db){
+    var autos = db.db('test').collection('autos');
+ 
+  assert.equal(null, err);
+   var cursor;
+
+ 
+ if (marka.length == 0 && location.length == 0)
+  {
+    cursor = autos.find({sedista:{$gte: p}, cena: {$gte: price1, $lte: price2}});
+
+  }
+else if(location.length ==0)
+{
+  cursor = autos.find({sedista:{$gte: p}, cena: {$gte: price1, $lte: price2}, marka:marka});
+}
+
+else if (marka.length == 0)
+{
+  cursor = autos.find({sedista:{$gte: Number(p)}, cena: {$gte: Number(price1), $lte: Number(price2)}, lokacija: String(location)});
+
+}
+  else {
+    cursor = autos.find({sedista:{$gte: Number(p)}, cena: {$gte: Number(price1), $lte: Number(price2)}, lokacija: String(location), 
+  marka: String(marka)});
+
+}
+
+
+ cursor.forEach(function(doc, err){
+  resultArray.push(doc);
+ },async function(){
+ var cars= [];
+ for(var b = 0; b<resultArray.length;++b)
+ {
+
+ 
+  
+ var e= await db.db('test').collection('bookings').find({$or: [{car: String(resultArray[b]._id), from: {$gte: Number(date1.getTime()), $lte: Number(date2.getTime())}},
+     {car: String(resultArray[b]._id), to: {$gte: Number(date1.getTime()), $lte: Number(date2.getTime())}}]}).count()
+  
+     if(e>0)
+   {
+     console.log('Bukirano');
+   
+   }
+   else{
+    
+    cars.push(resultArray[b]);
+    console.log(resultArray[b]);
+  }
+}
+db.close();
+    res.render('search', {cars: cars, from:from, to:to, people: people, location: location, cenaod: cenaod, cenado: cenado, marka: marka}); 
+ })
+   
+    
   })
+}
+})
 
 module.exports = router;
  
